3814d9032312a6e009c88d7627e1a2f8153b6ba9
Raditya <mp.raditya93w@gmail.com>
2025-03-17 00:03:28 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022330026/Program.cs
+++ tpmodul5_103022330026/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<long> dataNIM = new DataGeneric<long>(103022330026);
+
+        dataNIM.PrintData();
+    }
+}
